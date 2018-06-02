a789af9c4491594432d11ef0ede8a6a6a2b80787
Joseph Hill <joseph.hill1@outlook.com>
2018-06-02 10:57:00 -0400

    Scaffold authentication portion of app with necessary global config adjustments

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AuthenticationModule } from './authentication/authentication.module';
+import { AngularFireModule } from 'angularfire2';
+import { environment } from './environments/environment';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    CommonModule,
+    BrowserModule,
+    FormsModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    BrowserAnimationsModule,
+    RouterModule.forRoot([]),
+    AuthenticationModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [AngularFireAuth, AngularFireDatabase],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
